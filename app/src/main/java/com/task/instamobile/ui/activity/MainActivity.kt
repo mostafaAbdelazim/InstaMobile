24d29eed1d66b9312ebe3056f0927a50f3e02148
Mostafa Abdelazim <mzeemadev@gmail.com>
2019-10-05 17:25:34 +0200

    first commit

---
+++ app/src/main/java/com/task/instamobile/ui/activity/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.task.instamobile.ui.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import com.task.instamobile.R
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+    private lateinit var drawerLayout: DrawerLayout
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val toolbar = customToolbar
+        setSupportActionBar(toolbar)
+        drawerLayout = drawer_layout
+        navController = this.findNavController(R.id.nav_host_fragment)
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+        navView.setupWithNavController(navController)
+        navView.setNavigationItemSelectedListener { p0 ->
+            p0.isChecked = true
+            drawerLayout.closeDrawers()
+            when (p0.itemId) {
+                R.id.actionHome -> {
+                    if (navController.currentDestination?.id != R.id.actionHome) {
+                        navController.navigate(R.id.actionHome)
+                    }
+                }
+                R.id.actionCategories -> {
+                    if (navController.currentDestination?.id != R.id.actionCategories) {
+                        navController.navigate(R.id.actionCategories)
+                    }
+                }
+            }
+            true
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController, drawerLayout)
+    }
+}
