3c5e908d702ad51d990cdf41f76b20f90451d5b8
mostafaAbdelazim <mzeemadev@gmail.com>
2019-10-06 01:07:28 +0200

    zxc

--- app/src/main/java/com/task/instamobile/ui/fragment/home/HomeFragment.kt
+++ app/src/main/java/com/task/instamobile/ui/fragment/home/HomeFragment.kt
@@ -6,20 +6,29 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.google.firebase.firestore.FirebaseFirestore
 
 import com.task.instamobile.R
+import com.task.instamobile.databinding.FragmentHomeBinding
+import kotlinx.android.synthetic.main.fragment_home.*
 
-/**
- * A simple [Fragment] subclass.
- */
 class HomeFragment : Fragment() {
-
+    private lateinit var binding: FragmentHomeBinding
+    private val db = FirebaseFirestore.getInstance()
+    private val categories = db.collection("Categories").document("dZsWGCUvF6DlyJvF3r9H")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
+
+        categories.get().addOnSuccessListener {
+            text.text = it.data?.entries.toString()
+        }.addOnFailureListener {
+            text.text = it.localizedMessage
+        }
+        return binding.root
     }
 
 
